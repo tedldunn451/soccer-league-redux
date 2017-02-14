f6000801426ecc9223c94bd6b3114584802a4115
Ted Dunn <tldunn451@gmail.com>
2017-02-14 14:24:11 -0500

    Initial commit

---
+++ src/LeagueManager.java
@@ -0,0 +1,6 @@
+public class LeagueManager {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/teamtreehouse/soccer/League.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.soccer;
+
+/**
+ * Created by Ted on 2/14/2017.
+ */
+public class League {
+}
